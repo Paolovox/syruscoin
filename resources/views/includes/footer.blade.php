e143c400a7fbb56bb8078d0ebc6ff7954547252f
root <paolomondillo@gmail.com>
2017-12-17 22:26:53 +0100

    fix footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <footer id="footer" style="margin-top:5%;width:100%;text-align:right">
   <div class="row">
     <div class="col-lg-12">
-      <p>Made by <a href="http://thomaspark.co">Syrus Industry</a></p>
+      <p>Made by <a href="">Syrus Industry</a></p>
     </div>
   </div>
 </footer>
