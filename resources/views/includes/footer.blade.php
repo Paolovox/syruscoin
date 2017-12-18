e5707b3d281bd9439c1981631a12dd403c782b8b
root <paolomondillo@gmail.com>
2017-12-18 18:22:52 +0100

    fix footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
-<footer id="footer" style="margin-top:5%;width:100%;text-align:right;left:0px;bottom:0px;height:30px;">
+<footer id="footer" style="margin-top:5%;width:100%;text-align:right;position:fixed;left:-2%;bottom:0px;height:30px;">
   <div class="row">
     <div class="col-lg-12">
       <p>Made by <a href="">Syrus Industry</a></p>
